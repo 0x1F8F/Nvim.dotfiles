c800f5f1bd606e874a5f65da610a934f04f5774a
0x1F8F <86254906+0x1F8F@users.noreply.github.com>
2024-12-04 21:18:18 +0530

    nushell integration

--- lua/ext/ext.lua
+++ lua/ext/ext.lua
@@ -1,4 +1,50 @@
 return {
+{
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+        require("nvim-treesitter.configs").setup {
+            ensure_installed = { "nu" }, -- Ensure the "nu" parser is installed
+            highlight = {
+                enable = true,            -- Enable syntax highlighting
+            },
+            -- OPTIONAL!! These enable ts-specific textobjects.
+            -- So you can hit `yaf` to copy the closest function,
+            -- `dif` to clear the contet of the closest function,
+            -- or whatever keys you map to what query.
+            textobjects = {
+                select = {
+                    enable = true,
+                    keymaps = {
+                        -- You can use the capture groups defined in textobjects.scm
+                        -- For example:
+                        -- Nushell only
+                        ["aP"] = "@pipeline.outer",
+                        ["iP"] = "@pipeline.inner",
+
+                        -- supported in other languages as well
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["al"] = "@loop.outer",
+                        ["il"] = "@loop.inner",
+                        ["aC"] = "@conditional.outer",
+                        ["iC"] = "@conditional.inner",
+                        ["iS"] = "@statement.inner",
+                        ["aS"] = "@statement.outer",
+                    }, -- keymaps
+                }, -- select
+            }, -- textobjects
+        }
+    end,
+    dependencies = {
+        -- Additional Nushell parser
+        { "nushell/tree-sitter-nu", build = ":TSUpdate nu" },
+    },
+    build = ":TSUpdate",
+},
+
+
+
+
 	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 	'NvChad/nvim-colorizer.lua',
   'stevearc/dressing.nvim',
@@ -43,7 +89,6 @@ return {
       }
     end,
   },
-  { 'nvim-treesitter/nvim-treesitter' },
   { 'neovim/nvim-lspconfig' },
   { 'hrsh7th/cmp-nvim-lsp' },
   { 'hrsh7th/cmp-buffer' },
